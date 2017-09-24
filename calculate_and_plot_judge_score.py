a7f23aa3469899ce4a0295d03cd93e169ed73965
WuLC <liangchaowu5@gmail.com>
2017-09-24 22:15:14 +0800

    save the scores and visualize them

--- calculate_and_plot_judge_score.py
+++ calculate_and_plot_judge_score.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 # @Author: lc
 # @Date:   2017-09-23 21:31:59
-# @Last Modified by:   lc
-# @Last Modified time: 2017-09-23 21:41:17
+# @Last Modified by:   WuLC
+# @Last Modified time: 2017-09-24 22:13:29
 
 # get scores for result json files in a directory and plot the scores 
 # requires python2
 
 import os
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from caption_eval.run_evaluations import get_judge_score
 
 def get_batch_judge_score(reference_file, result_dir):
-    indices, scores = [], []
-    for filename in sorted(os.listdir(result_dir)):
-        num = filename.lstrip('result').rstrip('.json')
-        score = get_judge_score(result_dir + filename, reference_file)
-        indices.append(indices)
-        scores.append(score)
+    files = sorted(os.listdir(result_dir))
+    scores = [get_judge_score(result_dir + f, reference_file) for f in files]
 
+    np.array(scores).dump('../data/aichallenge/scores/scores.npy')
     # draw and save image
     fig = plt.figure()
-    plt.plot(indices, scores)
-    fig.savefig('../data/scores.png')
+    plt.plot(scores)
+    fig.savefig('../data/aichallenge/scores/scores.png')
+
+    print('max score was achieved by file: {0}'.format(files[scores.index(max(scores))]))
 
 
 if __name__ == '__main__':
