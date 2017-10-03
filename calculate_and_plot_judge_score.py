ca8260d5e329f444d138313354e9aaf58777d463
WuLC <liangchaowu5@gmail.com>
2017-10-03 10:41:15 +0800

    change the way of  dumping a numpy array

--- calculate_and_plot_judge_score.py
+++ calculate_and_plot_judge_score.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: lc
 # @Date:   2017-09-23 21:31:59
-# @Last Modified by:   WuLC
-# @Last Modified time: 2017-09-24 22:13:29
+# @Last Modified by:   LC
+# @Last Modified time: 2017-09-30 16:58:53
 
 # get scores for result json files in a directory and plot the scores 
 # requires python2
@@ -17,17 +17,17 @@ from caption_eval.run_evaluations import get_judge_score
 def get_batch_judge_score(reference_file, result_dir):
     files = sorted(os.listdir(result_dir))
     scores = [get_judge_score(result_dir + f, reference_file) for f in files]
-
-    np.array(scores).dump('../data/aichallenge/scores/scores.npy')
+    
+    np.savetxt('../data/aichallenge/scores/scores_custom_embedding.dat', np.array(scores))
     # draw and save image
     fig = plt.figure()
     plt.plot(scores)
-    fig.savefig('../data/aichallenge/scores/scores.png')
+    fig.savefig('../data/aichallenge/scores/scores_custom_embedding.png')
 
     print('max score was achieved by file: {0}'.format(files[scores.index(max(scores))]))
 
 
 if __name__ == '__main__':
     reference_file = '../data/aichallenge/annotations/captions_7500test.json'
-    result_dir = '../data/aichallenge/result/'
+    result_dir = '../data/aichallenge/result_custom_embedding/'
     get_batch_judge_score(reference_file, result_dir)
