e8308a82c2c6c56ebc77d7e7d1a5db1c06e6d93a
WuLC <liangchaowu5@gmail.com>
2017-10-03 11:40:47 +0800

    change code style

--- train.py
+++ train.py
@@ -14,9 +14,9 @@
 # ==============================================================================
 """Train the model."""
 
-import os 
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-os.environ["CUDA_VISIBLE_DEVICES"] = '1' # use the second GPU
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'  # use the second GPU
 
 import tensorflow as tf
 
@@ -48,7 +48,8 @@ tf.flags.DEFINE_string("custom_word_embedding", custom_word_embedding,
                        "Whether to use the word embedding file")
 tf.flags.DEFINE_boolean("train_inception", train_inception,
                         "Whether to train inception submodel variables.")
-tf.flags.DEFINE_integer("number_of_steps", num_steps, "Number of training steps.")
+tf.flags.DEFINE_integer("number_of_steps", num_steps,
+                        "Number of training steps.")
 tf.flags.DEFINE_integer("log_every_n_steps", 50,
                         "Frequency at which loss and global step are logged.")
 
@@ -56,77 +57,76 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 
 def main(unused_argv):
-  assert FLAGS.input_file_pattern, "--input_file_pattern is required"
-  assert FLAGS.trained_models_dir, "--trained_models_dir is required"
+    model_config = configuration.ModelConfig()
+    model_config.input_file_pattern = FLAGS.input_file_pattern
+    model_config.inception_checkpoint_file = FLAGS.inception_checkpoint_file
+    model_config.word_embedding_file = FLAGS.word_embedding_file
+    training_config = configuration.TrainingConfig()
 
-  model_config = configuration.ModelConfig()
-  model_config.input_file_pattern = FLAGS.input_file_pattern
-  model_config.inception_checkpoint_file = FLAGS.inception_checkpoint_file
-  model_config.word_embedding_file = FLAGS.word_embedding_file
-  training_config = configuration.TrainingConfig()
+    # Create training directory.
+    trained_models_dir = FLAGS.trained_models_dir
+    if not tf.gfile.IsDirectory(trained_models_dir):
+        tf.logging.info("Creating training directory: %s", trained_models_dir)
+        tf.gfile.MakeDirs(trained_models_dir)
 
-  # Create training directory.
-  trained_models_dir = FLAGS.trained_models_dir
-  if not tf.gfile.IsDirectory(trained_models_dir):
-    tf.logging.info("Creating training directory: %s", trained_models_dir)
-    tf.gfile.MakeDirs(trained_models_dir)
-
-  # Build the TensorFlow graph.
-  g = tf.Graph()
-  with g.as_default():
-    # Build the model, train from scratch
-    model = show_and_tell_model.ShowAndTellModel(
-            model_config, 
-            mode="train", 
+    # Build the TensorFlow graph.
+    g = tf.Graph()
+    with g.as_default():
+        # Build the model, train from scratch
+        model = show_and_tell_model.ShowAndTellModel(
+            model_config,
+            mode="train",
             train_inception=FLAGS.train_inception,
-            custom_word_embedding = FLAGS.custom_word_embedding)
-    model.build()
-    
-    # Set up the learning rate.
-    learning_rate_decay_fn = None
-    if FLAGS.train_inception:
-      learning_rate = tf.constant(training_config.train_inception_learning_rate)
-    else:
-      learning_rate = tf.constant(training_config.initial_learning_rate)
-      if training_config.learning_rate_decay_factor > 0:
-        num_batches_per_epoch = (training_config.num_examples_per_epoch /
-                                 model_config.batch_size)
-        decay_steps = int(num_batches_per_epoch *
-                          training_config.num_epochs_per_decay)
+            custom_word_embedding=FLAGS.custom_word_embedding)
+        model.build()
 
-        def _learning_rate_decay_fn(learning_rate, global_step):
-          return tf.train.exponential_decay(
-              learning_rate,
-              global_step,
-              decay_steps=decay_steps,
-              decay_rate=training_config.learning_rate_decay_factor,
-              staircase=True)
+        # Set up the learning rate.
+        learning_rate_decay_fn = None
+        if FLAGS.train_inception:
+            learning_rate = tf.constant(
+                training_config.train_inception_learning_rate)
+        else:
+            learning_rate = tf.constant(training_config.initial_learning_rate)
+            if training_config.learning_rate_decay_factor > 0:
+                num_batches_per_epoch = (training_config.num_examples_per_epoch /
+                                         model_config.batch_size)
+                decay_steps = int(num_batches_per_epoch *
+                                  training_config.num_epochs_per_decay)
 
-        learning_rate_decay_fn = _learning_rate_decay_fn
+                def _learning_rate_decay_fn(learning_rate, global_step):
+                    return tf.train.exponential_decay(
+                        learning_rate,
+                        global_step,
+                        decay_steps=decay_steps,
+                        decay_rate=training_config.learning_rate_decay_factor,
+                        staircase=True)
 
-    # Set up the training ops.
-    train_op = tf.contrib.layers.optimize_loss(
-        loss=model.total_loss,
+                learning_rate_decay_fn = _learning_rate_decay_fn
+
+        # Set up the training ops.
+        train_op = tf.contrib.layers.optimize_loss(
+            loss=model.total_loss,
+            global_step=model.global_step,
+            learning_rate=learning_rate,
+            optimizer=training_config.optimizer,
+            clip_gradients=training_config.clip_gradients,
+            learning_rate_decay_fn=learning_rate_decay_fn)
+
+        # Set up the Saver for saving and restoring model checkpoints.
+        saver = tf.train.Saver(
+            max_to_keep=training_config.max_checkpoints_to_keep)
+
+    # Run training.
+    tf.contrib.slim.learning.train(
+        train_op,
+        trained_models_dir,
+        log_every_n_steps=FLAGS.log_every_n_steps,
+        graph=g,
         global_step=model.global_step,
-        learning_rate=learning_rate,
-        optimizer=training_config.optimizer,
-        clip_gradients=training_config.clip_gradients,
-        learning_rate_decay_fn=learning_rate_decay_fn)
-
-    # Set up the Saver for saving and restoring model checkpoints.
-    saver = tf.train.Saver(max_to_keep=training_config.max_checkpoints_to_keep)
-
-  # Run training.
-  tf.contrib.slim.learning.train(
-      train_op,
-      trained_models_dir,
-      log_every_n_steps=FLAGS.log_every_n_steps,
-      graph=g,
-      global_step=model.global_step,
-      number_of_steps=FLAGS.number_of_steps,
-      init_fn=model.init_fn,
-      saver=saver)
+        number_of_steps=FLAGS.number_of_steps,
+        init_fn=model.init_fn,
+        saver=saver)
 
 
 if __name__ == "__main__":
-  tf.app.run()
+    tf.app.run()
