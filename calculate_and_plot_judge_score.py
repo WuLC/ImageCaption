184cd5ff91d88ea505ac73a41bfca11d5e516324
WuLC <liangchaowu5@gmail.com>
2018-01-18 14:06:15 +0800

    fix bug

--- calculate_and_plot_judge_score.py
+++ calculate_and_plot_judge_score.py
@@ -16,7 +16,13 @@ import matplotlib.pyplot as plt
 
 from caption_eval.run_evaluations import compute_m1
 
-log_file = './logs/prediction_json_scores_custom_embedding.log'
+reference_file = '../data/aichallenge/annotations/captions_7500test.json'
+result_dir = '../data/aichallenge/vgg_result/'
+# result_dir = '../data/aichallenge/inception_result/'
+# result_dir = '../data/aichallenge/inception_result_custom_embedding/'
+log_file = './logs/vgg_prediction_json_scores.log'
+# log_file = './logs/inception_prediction_json_scores.log'
+# log_file = './logs/inception_prediction_json_scores_custom_embedding.log'
 logging.basicConfig(level=logging.INFO, filename = log_file, filemode="a+", format="%(asctime)-15s %(levelname)-8s  %(message)s")
 
 
@@ -36,8 +42,10 @@ def get_batch_final_score(reference_file, result_dir):
     print('totally {0} result json files'.format(len(files)))
     final_scores = []
     evaluated_files = get_evaluted_files()
+    evaluated_count = 0
     for f in files:
         if f in evaluated_files:
+            evaluated_count += 1
             print('{0} has been caculated and recorded'.format(f))
             continue
         score = compute_m1(result_dir + f, reference_file)
@@ -59,13 +67,10 @@ def get_batch_final_score(reference_file, result_dir):
     """
     print('max score {0} was achieved by file: {1}'.format(
           max(final_scores),
-          files[final_scores.index(max(final_scores))]))
+          files[evaluated_count + final_scores.index(max(final_scores))]))
 
 
 if __name__ == '__main__':
-    reference_file = '../data/aichallenge/annotations/captions_7500test.json'
-    # result_dir = '../data/aichallenge/result/'
-    result_dir = '../data/aichallenge/result_custom_embedding/'
     get_batch_final_score(reference_file, result_dir)
     """
     predict_result = result_dir + 'result_model.ckpt-1202581.json'
